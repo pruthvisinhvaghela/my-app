e78ac1fae7fd1a373706c2e212a38bc21d72c427
pruthvisinhvaghela <pruthvivaghela19@gmail.com>
2024-02-05 15:20:33 +0530

    how to use props

--- src/App.js
+++ src/App.js
@@ -2,50 +2,13 @@ import "./App.css";
 import Child from "./Components/Child";
 
 function App() {
-  const people = [
-    {
-      id: 0,
-      name: "Creola Katherine Johnson",
-      profession: "mathematician",
-    },
-    {
-      id: 1,
-      name: "Mario José Molina-Pasquel Henríquez",
-      profession: "chemist",
-    },
-    {
-      id: 2,
-      name: "Mohammad Abdus Salam",
-      profession: "physicist",
-    },
-    {
-      id: 2,
-      name: "Mohammad Abdus Salam",
-      profession: "physicist",
-    },
-    {
-      name: "Percy Lavon Julian",
-      profession: "chemist",
-    },
-    {
-      name: "Subrahmanyan Chandrasekhar",
-      profession: "astrophysicist",
-    },
-  ];
-
-  const physicist = people.filter(
-    (person) => person.profession === "physicist"
-  );
-
-  let displayItems = physicist.map((person) => (
-    <>
-      <h1>the name is {person.name}</h1>
-      <br /> <h3> person id is {person.id}</h3>
-    </>
-  ));
   return (
     <>
-      <ul>{displayItems}</ul>
+      <h1>Spiced Chai Recipe</h1>
+      <h3>for two</h3>
+      <Child key={Child} water={2} />
+      <h3>for four</h3>
+      <Child key={Child} water={4} />
     </>
   );
 }
--- src/Components/Child.jsx
+++ src/Components/Child.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const Child = ({}) => {
-  return <></>;
+const Child = ({ water }) => {
+  return (
+    <>
+      <ol>
+        <li>Boil {water} cups of water.</li>
+        <li>
+          {" "}
+          Add {water} spoons of tea and {0.5 * water} spoons of spice.
+        </li>
+        <li>
+          Add {0.5 * water}
+          cups of milk to boil and sugar to taste.
+        </li>
+      </ol>
+    </>
+  );
 };
 
 export default Child;
